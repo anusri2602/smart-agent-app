1c1eeab0c2139e6752f5d1f3cbf12ae7edae38ff
anusri2602 <perecharla48@gmail.com>
2025-08-16 13:54:56 +0530

    Save local changes before rebase

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,13 +1,12 @@
-
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
-import { NhostClient } from '@nhost/nhost-js'
+import { createClient } from '@nhost/nhost-js'
 import { NhostProvider } from '@nhost/react'
 import { NhostApolloProvider } from '@nhost/react-apollo'
 import App from './App'
 
-const nhost = new NhostClient({
+const nhost = createClient({
   subdomain: import.meta.env.VITE_NHOST_SUBDOMAIN,
   region: import.meta.env.VITE_NHOST_REGION
 })
